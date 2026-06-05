514757d73be5f579a93e7f51df619522bd329433
DODDI LOKESH <shivakumargoud2008@gmail.com>
2026-06-05 10:14:42 +0530

    Add files via upload

---
+++ tasks/linkedlist.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class linkedlist{
+    public static void main(String args[]){
+        Node n1=new Node(10);
+        Node n2=new Node(20);
+        Node n3=new Node(30);
+        n1.next=n2;
+        n2.next=n3'
+        Node newnode=new Node();
+        newnode=n4;
+        int head=n4
+
+    }
+}
